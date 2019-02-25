270c0015c1e299233883d08184ef03773b837643
Marino <marinogomez24@gmail.com>
2019-02-25 00:38:02 -0400

    Create articles table, model, and validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
-    validates :title,presence: true
+    validates :title, presence: true, lenght: {minimum: 3, maximum: 50}
+    validates :description, presence: true, lenght: {minimum:10, maximum:300}
 end
