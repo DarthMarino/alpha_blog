6624e5f1abda846068068aac14f103b0cc617ce0
Marino <marinogomez24@gmail.com>
2019-04-11 20:57:59 -0400

    Added Partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -8,43 +11,41 @@ class ArticlesController < ApplicationController
   end
 
   def create
-
     @article = Article.new(article_params)
-    
     if @article.save
-    flash[:notice] = "Article was successfully created"
-    redirect_to article_path(@article)
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
     else
-    render 'new'
+      render 'new'
     end
   end
 
-  def update
-    @article = Article.find(params[:id]) 
-    if @article.update
+  def update   
+    if @article.update(article_params)
       flash[:notice] = "Article was succesfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
-  end
+    end
   end
 
   def show
-  @article = Article.find(params[:id])  
   end
 
   def edit
-  @article = Article.find(params[:id])
-  end   
-  
+  end 
+
   def destroy
-    @article= Article.find(params[:id])
     @article.destroy
     flash[:notice] ="Article was succesfully deleted"
     redirect_to articles_path
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title,:description)
     end
