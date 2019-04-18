8ea2ff9e4e1c4c6ad605904c147eba4b80aada26
Marino Ruby <marinogomez24@gmail.com>
2019-04-18 11:45:18 -0400

    navbar fix

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,14 +2,9 @@ $(document).on('click', '.notification > button.delete', function() {
     $(this).parent().addClass('is-hidden');
     return false;
 });
-$(document).ready(function() {
-
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
+$(document).on('click',function() {
   
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-  
-    });
-  });
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+});
