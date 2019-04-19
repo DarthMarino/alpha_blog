07b26cf7649fd777ffad6775658568162d4e871e
Marino <marinogomez24@gmail.com>
2019-04-18 22:13:40 -0400

    Recolored Messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   def flash_class(level)
     case level
         when 'success'then "notification is-success"
-        when 'alert' then "notification is-danger"
-        when 'deleted' then "notification is-info"
+        when 'deleted' then "notification is-danger"
+        when 'gucci' then "notification is-info"
     end
   end
 end
