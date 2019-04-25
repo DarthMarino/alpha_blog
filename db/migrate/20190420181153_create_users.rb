5a68a98b762224269a9e2ee12c3463292b62605a
Marino <marinogomez24@gmail.com>
2019-04-25 19:46:09 -0400

    Add User Data to articles listing page

--- db/migrate/20190420181153_create_users.rb
+++ db/migrate/20190420181153_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :before_save {self.email = email.downcase}
+      t.string :email
       t.timestamps
     end
   end
