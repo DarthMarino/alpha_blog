ae7dc7ba3c49313e28eff4c92cdd89ea5b4d3f8e
Marino <marinogomez24@gmail.com>
2019-05-22 12:55:39 -0400

    Authentication Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   include Pagy::Backend
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   def index
     @pagy, @users = pagy(User.all, page: params[:page], items: 5)
   end
@@ -31,11 +32,19 @@ class UsersController < ApplicationController
     @pagy, @users_articles = pagy(User.all, page: params[:page], items: 5)
   end
   private
+
   def user_params
     params.require(:user).permit(:username,:email,:password)  
   end
+
   def set_user
     @user = User.find(params[:id])
+  end
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
 end
     
